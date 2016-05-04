b87fef4ee7803c919723b2afa9c006f0ba686ab7
buyi <huaqirong@126.com>
2016-05-04 23:12:34 +0800

    create thread

---
+++ Chapter 1/ch1_recipe01/src/run.sh
@@ -0,0 +1,4 @@
+mkdir classes
+javac com/packtpub/java7/concurrency/chapter1/recipe1/core/Main.java -d classes
+javac com/packtpub/java7/concurrency/chapter1/recipe1/task/Calculator.java -d classes
+java  -cp classes com/packtpub/java7/concurrency/chapter1/recipe1/core/Main
