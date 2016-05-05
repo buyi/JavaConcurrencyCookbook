b1365587d3923620f446b5a2a3bd54bf1ffb733a
buyi <huaqirong@126.com>
2016-05-05 16:40:59 +0800

    interrupt thread

---
+++ Chapter 1/ch1_recipe03/src/run.sh
@@ -0,0 +1,4 @@
+mkdir classes
+javac com/packtpub/java7/concurrency/chapter1/recipe3/core/Main.java -d classes
+javac com/packtpub/java7/concurrency/chapter1/recipe3/task/PrimeGenerator.java -d classes
+java  -cp classes com/packtpub/java7/concurrency/chapter1/recipe3/core/Main
