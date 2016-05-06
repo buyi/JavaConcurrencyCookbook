dc5673086599aa7ad00374dd96000f452b2e3208
buyi <huaqirong@126.com>
2016-05-06 14:52:20 +0800

    thread pause and resume configuration

---
+++ Chapter 1/ch1_recipe05/src/run.sh
@@ -0,0 +1,8 @@
+# @Author: buyi
+# @Date:   2016-05-06 14:46:52
+# @Last Modified by:   buyi
+# @Last Modified time: 2016-05-06 14:47:19
+mkdir classes
+javac com/packtpub/java7/concurrency/chapter1/recipe5/core/Main.java -d classes
+javac com/packtpub/java7/concurrency/chapter1/recipe5/task/FileClock.java -d classes
+java  -cp classes com/packtpub/java7/concurrency/chapter1/recipe5/core/Main
