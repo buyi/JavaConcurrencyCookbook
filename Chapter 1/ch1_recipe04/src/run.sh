a90e0e3de86f774025866d115066da9d0d3c65be
buyi <huaqirong@126.com>
2016-05-06 11:18:58 +0800

    finish interupt logic

---
+++ Chapter 1/ch1_recipe04/src/run.sh
@@ -0,0 +1,4 @@
+mkdir classes
+javac com/packtpub/java7/concurrency/chapter1/recipe4/core/Main.java -d classes
+javac com/packtpub/java7/concurrency/chapter1/recipe4/task/FileSearch.java -d classes
+java  -cp classes com/packtpub/java7/concurrency/chapter1/recipe4/core/Main
